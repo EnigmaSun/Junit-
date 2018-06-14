950858f80ae07d4fc8764129cb9e944a429e6fc2
EnigmaSun <szy_personal@163.com>
2018-06-14 15:37:32 +0800

    fix some mistakes

--- src/java/Myuitl/SimpleCalculation.java
+++
@@ -1,9 +0,0 @@
-package  Myuitl;
-
-public class SimpleCalculation {
-    public int Add(int a,int b){        
-        return (a + b);        
-    }
-
-}
-
--- src/test/MyutilTest/SimpleCalculationTest.java
+++ src/test/MyutilTest/SimpleCalculationTest.java
@@ -1,29 +1,9 @@
-<<<<<<< HEAD
 package test.MyutilTest;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 import code.Myutil.*;
-
-public class SimpleCalculationTest {
-
-	SimpleCalculation sc = new SimpleCalculation();
-
-	@Test
-	public void AddTest() {
-		int c = sc.Add(1, 1);
-		assertEquals(2, c);
-	}
-}
-=======
-package MyutilTest;
-
-import Myuitl.*;
-
-import static org.junit.Assert.*;
-import org.junit.Test;
 
 public class SimpleCalculationTest {
 
@@ -38,4 +18,3 @@ public class SimpleCalculationTest {
     }
 }
 
->>>>>>> ac5924006fdc85bc8faa133b9e67e0ff810d4d45
