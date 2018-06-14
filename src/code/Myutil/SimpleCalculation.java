ab8c2c70304ca5b31bbff4e69b98736b57fabeea
EnigmaSun <szy_personal@163.com>
2018-06-14 15:01:20 +0800

    build in eclipse

---
+++ src/code/Myutil/SimpleCalculation.java
@@ -0,0 +1,9 @@
+package  code.Myutil;
+
+public class SimpleCalculation {
+    public int Add(int a,int b){        
+        return (a + b);        
+    }
+
+}
+
--- src/test/MyutilTest/SimpleCalculationTest.java
+++ src/test/MyutilTest/SimpleCalculationTest.java
@@ -1,3 +1,23 @@
+<<<<<<< HEAD
+package test.MyutilTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import code.Myutil.*;
+
+public class SimpleCalculationTest {
+
+	SimpleCalculation sc = new SimpleCalculation();
+
+	@Test
+	public void AddTest() {
+		int c = sc.Add(1, 1);
+		assertEquals(2, c);
+	}
+}
+=======
 package MyutilTest;
 
 import Myuitl.*;
@@ -18,3 +38,4 @@ public class SimpleCalculationTest {
     }
 }
 
+>>>>>>> ac5924006fdc85bc8faa133b9e67e0ff810d4d45
